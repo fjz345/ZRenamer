4a442f931ab95dd12d930282092aaadb33d45eff
Filip Zachrisson <fjz345@hotmail.com>
2020-06-17 20:25:00 +0200

    Working prototype

---
+++ ZRenamer/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ZRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.tbRegex = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbFilterTXT = new System.Windows.Forms.CheckBox();
+            this.cbFilterMKV = new System.Windows.Forms.CheckBox();
+            this.lvFound = new System.Windows.Forms.ListView();
+            this.tbRegexTest = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(12, 89);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(299, 20);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(12, 202);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(299, 20);
+            this.tbOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input name(s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output name(s)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(314, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Found files in same dir";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 462);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(576, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(104, 60);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFile.TabIndex = 7;
+            this.btnChooseFile.Text = "Choose file";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(209, 415);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(189, 41);
+            this.btnRename.TabIndex = 8;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // tbRegex
+            // 
+            this.tbRegex.Location = new System.Drawing.Point(12, 308);
+            this.tbRegex.Name = "tbRegex";
+            this.tbRegex.Size = new System.Drawing.Size(358, 20);
+            this.tbRegex.TabIndex = 9;
+            this.tbRegex.TextChanged += new System.EventHandler(this.tbRegex_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 292);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Regex";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbFilterTXT);
+            this.groupBox1.Controls.Add(this.cbFilterMKV);
+            this.groupBox1.Location = new System.Drawing.Point(404, 309);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 147);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // cbFilterTXT
+            // 
+            this.cbFilterTXT.AutoSize = true;
+            this.cbFilterTXT.Location = new System.Drawing.Point(6, 42);
+            this.cbFilterTXT.Name = "cbFilterTXT";
+            this.cbFilterTXT.Size = new System.Drawing.Size(47, 17);
+            this.cbFilterTXT.TabIndex = 1;
+            this.cbFilterTXT.Text = "TXT";
+            this.cbFilterTXT.UseVisualStyleBackColor = true;
+            this.cbFilterTXT.CheckedChanged += new System.EventHandler(this.cbFilterTXT_CheckedChanged);
+            // 
+            // cbFilterMKV
+            // 
+            this.cbFilterMKV.AutoSize = true;
+            this.cbFilterMKV.Location = new System.Drawing.Point(6, 19);
+            this.cbFilterMKV.Name = "cbFilterMKV";
+            this.cbFilterMKV.Size = new System.Drawing.Size(49, 17);
+            this.cbFilterMKV.TabIndex = 0;
+            this.cbFilterMKV.Text = "MKV";
+            this.cbFilterMKV.UseVisualStyleBackColor = true;
+            this.cbFilterMKV.CheckedChanged += new System.EventHandler(this.cbFilterMKV_CheckedChanged);
+            // 
+            // lvFound
+            // 
+            this.lvFound.GridLines = true;
+            this.lvFound.HideSelection = false;
+            this.lvFound.Location = new System.Drawing.Point(327, 73);
+            this.lvFound.Name = "lvFound";
+            this.lvFound.ShowGroups = false;
+            this.lvFound.Size = new System.Drawing.Size(261, 220);
+            this.lvFound.TabIndex = 13;
+            this.lvFound.UseCompatibleStateImageBehavior = false;
+            this.lvFound.View = System.Windows.Forms.View.List;
+            // 
+            // tbRegexTest
+            // 
+            this.tbRegexTest.Enabled = false;
+            this.tbRegexTest.Location = new System.Drawing.Point(12, 334);
+            this.tbRegexTest.Name = "tbRegexTest";
+            this.tbRegexTest.Size = new System.Drawing.Size(86, 20);
+            this.tbRegexTest.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(101, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Should see only the episode number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 497);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbRegexTest);
+            this.Controls.Add(this.lvFound);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbRegex);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.TextBox tbRegex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbFilterTXT;
+        private System.Windows.Forms.CheckBox cbFilterMKV;
+        private System.Windows.Forms.ListView lvFound;
+        private System.Windows.Forms.TextBox tbRegexTest;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ZRenamer/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ZRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbRegex.Text = patternEpisodeNr;
+        }
+
+        private void UpdateFoundFiles()
+        {
+            List<string> foundFiles = new List<string>();
+            // Find all episodes in same dir with extensions
+            try
+            {
+                // check so that the file is numerable
+                string[] files = Directory.GetFiles(directory);
+                foreach (string f in files)
+                {
+                    Match match = Regex.Match(f, patternEpisodeNr);
+
+                    if (!match.Success)
+                    {
+                        // skip
+                    }
+                    else
+                    {
+                        foundFiles.Add(f);
+                    }
+                }
+                Console.WriteLine("The number of files starting with is {0}.", foundFiles.Count);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("The process (finding files) failed: {0}", err.ToString());
+            }
+
+
+            lvFound.Items.Clear();
+            // Add all entries to the list
+            foreach (string dir in foundFiles)
+            {
+                Console.WriteLine(dir);
+
+                lvFound.Items.Add(Path.GetFileName(dir));
+
+            }
+
+            // Mark Filtered Items
+            MarkFilteredItems();
+        }
+
+        public void MarkFilteredItems()
+        {
+            for(int i = 0; i < lvFound.Items.Count; i++)
+            {
+                lvFound.Items[i].BackColor = Color.Green;
+
+                foreach (var filter in map)
+                {
+                    if (filter.Value == false)
+                        continue;
+
+                    if ('.' + filter.Key == Path.GetExtension(lvFound.Items[i].Text))
+                    {
+                        lvFound.Items[i].BackColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        public void ProcessInput()
+        {
+            string file = tbInput.Text;
+
+            if (file == "")
+                return;
+
+            // Set working directory to the dir
+            try
+            {
+                directory = Path.GetDirectoryName(file);
+                inputFileName = Path.GetFileName(file);
+                Directory.SetCurrentDirectory(directory);
+            }
+            catch (System.ArgumentException)
+            {
+                return;
+            }
+            
+
+            // Update foundFiles
+            UpdateFoundFiles();
+
+
+
+
+            // Format the input string to include number wildcards "hejsan01.txt" --> "hejsan??.txt"
+            MatchEvaluator evaluator = new MatchEvaluator(ReplaceNum);
+            string f_filename = Regex.Replace(inputFileName, patternDigit, evaluator);
+            Console.WriteLine("Formated name: " + f_filename);
+
+            // Set output filename without extension
+            tbOutput.Text = Path.GetFileNameWithoutExtension(f_filename);
+
+
+            PreviewRegex(inputFileName);
+        }
+
+        public void PreviewRegex(string previewText)
+        {
+            // Get the specific number
+            Match match = Regex.Match(previewText, patternEpisodeNr);
+
+            string nr = match.Groups["episode"].Value;
+            if (!match.Success)
+            {
+                Console.WriteLine("Preview Match Not successfull");
+                tbRegexTest.Text = "<Failed>";
+                return;
+            }
+
+            tbRegexTest.Text = nr;
+        }
+
+        public static string ReplaceNum(Match match)
+        {
+            return @"?";
+        }
+        public static string RemoveNum(Match match)
+        {
+            return @"";
+        }
+
+        public static string nrToReplace = "";
+        public static string ReplaceQ(Match match)
+        {
+            Console.WriteLine(match.Value);
+            return nrToReplace;
+        }
+
+        string directory = "";
+        string inputFileName = "";
+        string patternDigit = @"\d";
+        string patternEpisodeNr = @"(- )(?'episode'\d+)";
+        string patternNewName = @"\?+";
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            string file = "";
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+
+                tbInput.Text = file;
+                Console.WriteLine(file); // <-- Shows file size in debugging mode.
+            }
+
+            ProcessInput();
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (lvFound.Items.Count == 0)
+            {
+                return;
+            }
+
+
+
+            // Rename all the files
+            for (int i = 0; i < lvFound.Items.Count; i++)
+            {
+                // Get the specific number
+                Match match = Regex.Match(lvFound.Items[i].ToString(), patternEpisodeNr);
+
+                string nr = match.Groups["episode"].Value;
+                if (!match.Success)
+                {
+                    Console.WriteLine("Match Not successfull");
+                    return;
+                }
+
+                nrToReplace = nr;
+                MatchEvaluator evaluatorNewName = new MatchEvaluator(ReplaceQ);
+                string newFileName = Regex.Replace(tbOutput.Text, patternNewName, evaluatorNewName);
+
+                // Rember that the extension is not in the string
+                File.Move(lvFound.Items[i].Text, newFileName + Path.GetExtension(lvFound.Items[i].Text));
+
+                Console.WriteLine(newFileName);
+            }
+
+        }
+
+
+        Dictionary<string, bool> map = new Dictionary<string, bool>()
+        {
+            {"mkv", false},
+            {"txt", false},
+        };
+            
+        private void cbFilterMKV_CheckedChanged(object sender, EventArgs e)
+        {
+            map["mkv"] = cbFilterMKV.Checked;
+            MarkFilteredItems();
+        }
+
+        private void cbFilterTXT_CheckedChanged(object sender, EventArgs e)
+        {
+            map["txt"] = cbFilterTXT.Checked;
+            MarkFilteredItems();
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                ProcessInput();
+            }
+        }
+
+        private void tbRegex_TextChanged(object sender, EventArgs e)
+        {
+            PreviewRegex(inputFileName);
+        }
+    }
+}
